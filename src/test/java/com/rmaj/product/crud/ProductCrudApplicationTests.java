d8adaa5b6b7fe481aaef83b55da90446afce5e69
Maniar Meet <meetrmaniar@gmail.com>
2020-09-30 22:08:43 -0400

    2nd commit

---
+++ src/test/java/com/rmaj/product/crud/ProductCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmaj.product.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
